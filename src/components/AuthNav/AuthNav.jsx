68f6165966f57bb4dad5dbe5b4e6ec7efa07c21c
ArturMandryk <ArturMandryk@gmail.com>
2022-11-25 15:07:13 +0200

    1

--- src/components/AuthNav/AuthNav.jsx
+++
@@ -1,9 +0,0 @@
-import { Container, Link } from './AuthNav.styled';
-export const AuthNav = () => {
-  return (
-    <Container>
-      <Link to="/register">Register </Link>
-      <Link to="/login">Log In</Link>
-    </Container>
-  );
-};
